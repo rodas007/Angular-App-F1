8654842167b4caf7802461a17a555bc0c64d7a5d
rodas007 <sergioproba@gmail.com>
2022-04-01 11:03:25 +0200

    FIX footer

--- src/app/pages/drivers-page/page/drivers-detail/drivers-detail.module.ts
+++ src/app/pages/drivers-page/page/drivers-detail/drivers-detail.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DriversDetailRoutingModule } from './drivers-detail-routing.module';
 import { DriversDetailComponent } from './drivers-detail.component';
-
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -11,7 +11,8 @@ DriversDetailComponent
   ],
   imports: [
     CommonModule,
-    DriversDetailRoutingModule
+    DriversDetailRoutingModule,
+    ButtonModule
   ]
 })
 export class DriversDetailModule { }
